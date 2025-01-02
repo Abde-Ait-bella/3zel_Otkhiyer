d67f03ac26b19735e7a13c827336f14fa0ec0a3a
Abdessamad Ait-bella <abdessamadaitbella1998@gmail.com>
2025-01-03 00:21:47 +0100

    feat: la structure mvc

--- oopCrud/products/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if($_GET['id']){
-    require_once 'classes/ProductManager.php';
-    $productManager = new ProductManager();
-    $productManager->delete($_GET['id']);
-    header('Location: /products/index.php');
-}
